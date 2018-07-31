627a7a84ae689e9e9ffea9efc0cc6c983feb69b9
Walter O'Keefe <walterokeefe@posteo.net>
2018-08-01 00:41:26 +0930

    Add test file

---
+++ test.adb
@@ -0,0 +1,11 @@
+procedure Something(X : in Integer; Y : in out Positive) is
+   A : Integer := 2#0101_1001;
+   B : Integer := 16#CaFe_F00d#E+10;
+   C : Integer := Some_Function_Call(0);
+   D : Integer := 500;
+   E : Float   := 1_2_3_4.5_6_7_8e-1_2_3_4;
+   F : Matrix3 := 0;
+   G : Type_5  := Value_4_3_2_1(0);
+begin
+   null;
+end Something;
